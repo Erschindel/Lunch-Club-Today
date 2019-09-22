3f54f6d16724ff29c150113b025902e7fadfc751
Erschindel <erschindel@gmail.com>
2019-09-21 20:39:55 -0400

    Added input for locatinos, need to connect to places lib

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -5,13 +5,40 @@ class Maps extends React.Component{
 
   constructor(props){
     super(props);
+
+    this.state = {
+      input: "",
+      submit: ""
+    }
   }
+
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      submit: this.state.input
+    })
+  };
 
   render(){
     //
     return(
       <div>
-          <Map google={this.props.google} zoom={11}>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" onChange={this.handleChange} value={this.state.value} placeholder="Enter location (i.e \'Chipotle\', \'&pizza\', etc.)"/>
+            <button type="submit">Find</button>
+          </form>
+          <h1>{this.state.submit}</h1>
+          <Map
+            google={this.props.google}
+            zoom={12}
+            // initialCenter={lat: 39, lng: -70}
+            >
             <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
             <InfoWindow onClose={this.onInfoWindowClose}>
@@ -26,3 +53,13 @@ class Maps extends React.Component{
 export default GoogleApiWrapper({
   apiKey: ("AIzaSyCceXdLpZ5b6QkArMR73G59M9muGJgSQdg")
 })(Maps)
+
+Maps.defaultProps = {
+  zoom: 14,
+  initialCenter: {
+    lat: -1.2884,
+    lng: 36.8233
+  },
+  centerAroundCurrentLocation: false,
+  visible: true
+};
