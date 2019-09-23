41d8b4f7a754b8c6293877fd5fcfe5615863d5ab
Erschindel <erschindel@gmail.com>
2019-09-23 01:06:21 -0400

    Minor updates, improved readability, started styles

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,23 +1,58 @@
 import React from "react";
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import "../App.css";
 
 class Maps extends React.Component{
 
   constructor(props){
     super(props);
+
+    this.state = {
+      input: "",
+      submit: ""
+    }
   }
+
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      submit: this.state.input
+    })
+  };
 
   render(){
     //
     return(
       <div>
-          <Map google={this.props.google} zoom={11}>
-            <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
-            <InfoWindow onClose={this.onInfoWindowClose}>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" onChange={this.handleChange} value={this.state.value} placeholder="Enter location (i.e \'Chipotle\', \'&pizza\', etc.)"/>
+            <button type="submit">Find</button>
+          </form>
+          <h1>{this.state.submit}</h1>
+          <div>
+            <Map
+              google={this.props.google}
+              zoom={13}
+              initialCenter={{
+                lat: 39.1887131,
+                lng: -77.2627294
+              }}
+              className={"map-sizing"}
 
-            </InfoWindow>
-          </Map>
+              >
+              <Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />
+              <InfoWindow onClose={this.onInfoWindowClose}>
+
+              </InfoWindow>
+            </Map>
+          </div>
       </div>
     )
   }
--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../App.css";
 
 class Timer extends React.Component{
   constructor(props){
@@ -6,12 +7,37 @@ class Timer extends React.Component{
 
     let current = new Date(),
       lunchDay = [3,12,38,42],
+
+
       // setting minutes, hours, and days relative to adjusted seconds s
 
-      s = (current.getSeconds() > lunchDay[3]) ? (59 + lunchDay[3] - current.getSeconds()) : (lunchDay[3] - current.getSeconds()),
-      m = (s > lunchDay[3]) ? ((current.getMinutes() > lunchDay[2]) ? (60 + lunchDay[2] - current.getMinutes() - 1) : (lunchDay[2] - current.getMinutes()) - 1) : ((current.getMinutes() > lunchDay[2]) ? (60 + lunchDay[2] - current.getMinutes()) : (lunchDay[2] - current.getMinutes())),
-      h = (m > lunchDay[2]) ? ((current.getHours() > lunchDay[1]) ? (24 + lunchDay[1] - current.getHours() - 1) : (lunchDay[1] - current.getHours()) - 1) : ((current.getHours() > lunchDay[1]) ? (24 + lunchDay[1] - current.getHours()) : (lunchDay[1] - current.getHours())),
-      d = (h > lunchDay[1]) ? ((current.getDay() > lunchDay[0]) ? (7 + lunchDay[0] - current.getDay() - 1) : (lunchDay[0] - current.getDay()) - 1) : ((current.getDay() > lunchDay[0]) ? (7 + lunchDay[0] - current.getDay()) : (lunchDay[0] - current.getDay()));
+      s = (current.getSeconds() > lunchDay[3]) ?
+            (59 + lunchDay[3] - current.getSeconds()) :
+            (lunchDay[3] - current.getSeconds()),
+
+      m = (s > lunchDay[3]) ?
+            ((current.getMinutes() > lunchDay[2]) ?
+              (60 + lunchDay[2] - current.getMinutes() - 1) :
+              (lunchDay[2] - current.getMinutes()) - 1) :
+            ((current.getMinutes() > lunchDay[2]) ?
+              (60 + lunchDay[2] - current.getMinutes()) :
+              (lunchDay[2] - current.getMinutes())),
+
+      h = (m > lunchDay[2]) ?
+            ((current.getHours() > lunchDay[1]) ?
+              (24 + lunchDay[1] - current.getHours() - 1) :
+              (lunchDay[1] - current.getHours()) - 1) :
+            ((current.getHours() > lunchDay[1]) ?
+              (24 + lunchDay[1] - current.getHours()) :
+              (lunchDay[1] - current.getHours())),
+
+      d = (h > lunchDay[1]) ?
+            ((current.getDay() > lunchDay[0]) ?
+              (7 + lunchDay[0] - current.getDay() - 1) :
+              (lunchDay[0] - current.getDay()) - 1) :
+            ((current.getDay() > lunchDay[0]) ?
+              (7 + lunchDay[0] - current.getDay()) :
+              (lunchDay[0] - current.getDay()));
 
     this.state = {
       days: d,
@@ -22,6 +48,9 @@ class Timer extends React.Component{
 
   }
 
+
+  // lifecycle methods to refresh clock
+
   componentDidMount(){
     this.interval = setInterval(this.getRemainingTime, 1000);
   }
@@ -30,14 +59,51 @@ class Timer extends React.Component{
     clearInterval(this.interval);
   }
 
+
+  // set number styles to color based on relevance; if days = 0, hours is colored and enlarged
+
+  setTimerDays = () => {
+    return (this.state.days > 0) ? "timerDaysOn" : ""
+  }
+
+  setTimerHours = () => {
+    return (this.state.hours > 0 && this.state.days === 0) ? "timerHoursOn" : ""
+  }
+
+  setTimerMins = () => {
+    return (this.state.minutes > 0 && this.state.days === 0 && this.state.hours === 0) ? "timerMinsOn" : ""
+  }
+
+  setTimerSecs = () => {
+    return (this.state.seconds > 0 && this.state.days === 0 && this.state.hours === 0 && this.state.minutes === 0) ? "timerSecsOn" : ""
+  }
+
+
+  // remove "s"; if days = 1, display "1 day" instead of "1 days"
+
+  daysS = () => {
+    return (this.state.days === 1) ? "" : "s";
+  }
+
+  hoursS = () => {
+    return (this.state.hours === 1) ? "" : "s";
+  }
+
+  minsS = () => {
+    return (this.state.minutes === 1) ? "" : "s";
+  }
+
+  secsS = () => {
+    return (this.state.seconds === 1) ? "" : "s";
+  }
+
   getRemainingTime = () => {
     const today = new Date();
     let hooray = false,
       lunchDay = [3,12,38,42],
       secs = (today.getSeconds() > lunchDay[3]) ? (60 + lunchDay[3] - today.getSeconds()) : (lunchDay[3] - today.getSeconds()),
-      //set minutes to decrement with respect to seconds, if minutes is > 0
+      //set minutes to decrement with respect to seconds, if minutes is > 0; otherwise, reset minutes and decrement hours
       mins = (this.state.minutes !== -1) ? ((secs === 59) ? (this.state.minutes - 1) : (this.state.minutes)) : (59),
-      //set hours to decrement with respect to minutes, if hours is > 0
       hours = (this.state.hours !== -1) ? ((mins === 59 && secs === 58) ? (this.state.hours - 1) : (this.state.hours)) : (23),
       day = (this.state.days !== -1) ? ((hours === 23 && mins === 59 && secs === 57) ? (this.state.days - 1) : (this.state.days)) : (6);
 
@@ -54,10 +120,15 @@ class Timer extends React.Component{
   }
 
   render(){
-    //
+
     return(
       <div>
-        <span>{this.state.days} days, {this.state.hours} hours, {this.state.minutes} minutes, and {this.state.seconds} seconds until next out to lunch!</span>
+        <p>
+          <span className={this.setTimerDays()}> {this.state.days}</span> day{this.daysS()},
+          <span className={this.setTimerHours()}> {this.state.hours}</span> hour{this.hoursS()},
+          <span className={this.setTimerMins()}> {this.state.minutes}</span> minute{this.minsS()}, and
+          <span className={this.setTimerSecs()}> {this.state.seconds}</span> second{this.secsS()} until next out to lunch!
+        </p>
       </div>
     )
   }
